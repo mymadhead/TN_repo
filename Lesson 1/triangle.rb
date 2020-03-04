aaf729b7f7d7db8fc09c98139c8b5d66d38e9eda
mymadhead <mymadhead@gmail.com>
2020-03-05 00:56:02 +0300

    lesson 1

--- Lesson 1/triangle.rb
+++ Lesson 1/triangle.rb
@@ -1,6 +1,6 @@
 # Area of a triangle. User enters the sides of triangle and get the area of a triangle.
 puts 'Enter the side a of the triangle:'
-a = gets.chomp.to_f # if we use a "to_i" method or another "to...",or don't use it, our results will be not exact. Or program will don't work properly. 
+a = gets.chomp.to_f
 puts 'Enter the side b of the triangle:'
 b = gets.chomp.to_f
 triangle_area = 0.5 * a * b
