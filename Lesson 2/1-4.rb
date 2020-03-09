4962df3be2925c97b37fbcbaccae26797d669125
mymadhead <mymadhead@gmail.com>
2020-03-09 14:47:37 +0300

    Lesson 2

---
+++ Lesson 2/1-4.rb
@@ -0,0 +1,15 @@
+# Сделать хеш, содеращий месяцы и количество дней в месяце. В цикле выводить те месяцы, у которых количество дней ровно 30
+calendar = { 
+    january: 30,
+    february: 28,
+    march: 31,
+    april: 31,
+    may: 31,
+    june: 31,
+    july: 31,
+    august: 31,
+    september: 31,
+    october: 31,
+    november: 31,
+    december: 31
+}
