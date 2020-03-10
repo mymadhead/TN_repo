c64053b4508657f55f38ccb91210dfd618b37601
mymadhead <mymadhead@gmail.com>
2020-03-10 21:07:49 +0300

    Lesson 2

--- Lesson 2/3.rb
+++ Lesson 2/3.rb
@@ -1,8 +1,7 @@
 # Заполнить массив числами фибоначчи до 100
 fibonacci = [0, 1]
 next_num = 1
-until next_num >= 100
+while (next_num = fibonacci[-1] + fibonacci[-2]) <= 100
   fibonacci << next_num
-  next_num = fibonacci[-1] + fibonacci[-2]
 end
 puts "Fibonacci numbers: #{fibonacci}"
--- Lesson 2/4.rb
+++ Lesson 2/4.rb
@@ -1,6 +1,6 @@
 # Заполнить хеш гласными буквами, где значением будет являтся порядковый номер буквы в алфавите (a - 1).
 hash = {}
 alphabet = ('a'..'z').to_a
-glasnie = %w[a e i o u]
-alphabet.each.with_index(1) { |e, i| hash[e] = i if glasnie.include? e }
+vowels = %w[a e i o u]
+alphabet.each.with_index(1) { |letter, i| hash[letter] = i if vowels.include? letter }
 puts hash
