18530c58ace47d17adf9a131acd59d49f72a973c
mymadhead <mymadhead@gmail.com>
2020-03-13 16:20:25 +0300

    Lesson 3

--- Lesson 3/station.rb
+++ Lesson 3/station.rb
@@ -1,1 +1,21 @@
 # Station class
+# Имеет название, которое указывается при ее создании
+# Может принимать поезда (по одному за раз)
+# Может возвращать список всех поездов на станции, находящиеся в текущий момент
+# Может возвращать список поездов на станции по типу (см. ниже): кол-во грузовых, пассажирских
+# Может отправлять поезда (по одному за раз, при этом, поезд удаляется из списка поездов, находящихся на станции).
+class Station
+  attr_reader :trains, :name
+  def initialize(name)
+    @name = name
+    @trains = []
+  end
+
+  def arrive_train(train)
+    @trains << train
+  end
+
+  def to_s
+    train.each { |train| puts "#{name} station: #{train.train_number}, #{train.type}" }
+  end
+end
