be8563aa3f1a4310258c1649a2b407f0dda0dac6
mymadhead <mymadhead@gmail.com>
2020-03-19 20:51:56 +0300

    Lesson 3

--- Lesson 3/station.rb
+++ Lesson 3/station.rb
@@ -5,21 +5,5 @@
 # Может возвращать список поездов на станции по типу (см. ниже): кол-во грузовых, пассажирских
 # Может отправлять поезда (по одному за раз, при этом, поезд удаляется из списка поездов, находящихся на станции).
 class Station
-  attr_reader :trains, :name
-  def initialize(name)
-    @name = name
-    @trains = []
-  end
 
-  def arrive_train(train)
-    @trains << train
-  end
-
-  def depart_train(train)
-    @trains.delete(train)
-  end
-  
-  def to_s
-    @trains.each { |train| puts "#{name} station: #{train.train_number}, #{train.type}" }
-  end
 end
