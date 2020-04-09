af53fbb5ff2d73a1c5da4b888691477d92b8a845
mymadhead <mymadhead@gmail.com>
2020-04-09 19:04:38 +0300

    Add seed.rb and some fixes

--- Lesson 7/interface.rb
+++ Lesson 7/interface.rb
@@ -1,12 +1,3 @@
-require_relative 'train'
-require_relative 'passenger_train'
-require_relative 'cargo_train'
-require_relative 'station'
-require_relative 'route'
-require_relative 'wagon'
-require_relative 'cargo_wagon'
-require_relative 'passenger_wagon'
-
 class Interface
 
   def start
